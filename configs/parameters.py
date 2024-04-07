2bb1fa24dc1adab057a4578a529859f420be8d5f
Wesley Carneiro <69411274+Wesley-scarneiro@users.noreply.github.com>
2024-04-07 01:10:02 -0300

    chore: simplifications in perceptron (#1)

--- configs/parameters.py
+++ configs/parameters.py
@@ -1,10 +1,5 @@
-from random import uniform
-
 class Parameters:
 
     learning_rate = 0.0
-    bias_value = 1
-    
-    @staticmethod
-    def _weight_random() -> float:
-        return round(uniform(0, 1), 3)
+    bias_value = 0
+    inputs_total = 2
